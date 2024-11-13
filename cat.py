f1882cd7750022576ad8d755086be09a716fbc29
meryemyigit2004 <yigitmeryem540@gmail.com>
2024-11-13 21:24:22 +0300

    ilk commit

---
+++ cat.py
@@ -0,0 +1,7 @@
+class cat:
+    def __init__(self,name,genus,age):
+        self.name = name
+        self. genus = genus
+        self.age = age
+def info(self):
+    print(f'{self.name}\n{self.genus}\n{self.age}')
